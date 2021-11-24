d3e64e340924a6d88ddd15a7720f897269782f46
Junaid <sj.shaikhjunaid@gmail.com>
2021-11-24 20:09:27 +0530

    add animal and breeds dropdown

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,7 +1,13 @@
 import { useState } from "react";
 
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
+
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  const [disabled, setDisabled] = useState(true);
+  const breeds = [];
 
   return (
     <div className="search-params">
@@ -14,6 +20,40 @@ const SearchParams = () => {
             placeholder="Location"
             onChange={(e) => setLocation(e.target.value)}
           />
+        </label>
+
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => {
+              setAnimal(e.target.value);
+              setDisabled(false);
+            }}
+            onBlue={(e) => setAnimal(e.target.value)}
+          >
+            <option />
+            {ANIMALS.map((animal) => {
+              return <option value={animal}>{animal}</option>;
+            })}
+          </select>
+        </label>
+
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            value={animal}
+            onChange={(e) => setBreed(e.target.value)}
+            onBlue={(e) => setBreed(e.target.value)}
+            disabled={disabled}
+          >
+            <option />
+            {breeds.map((breed) => {
+              return <option value={breed}>{breed}</option>;
+            })}
+          </select>
         </label>
         <button type="submit">Submit</button>
       </form>
