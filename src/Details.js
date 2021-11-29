947f4cd38752a526ae6d34059de8151a1eab84ad
Junaid <sj.shaikhjunaid@gmail.com>
2021-11-29 16:51:31 +0530

    rebuid Details component with classes

--- src/Details.js
+++ src/Details.js
@@ -1,9 +1,44 @@
-import { useParams } from "react-router-dom";
+import { Component } from "react";
+import { withRouter } from "react-router-dom";
 
-const Details = () => {
-  const { id } = useParams();
+class Details extends Component {
+  constructor(props) {
+    super(props);
 
-  return <h2>{id}</h2>;
-};
+    this.state = {
+      loading: true,
+    };
+  }
 
-export default Details;
+  async componentDidMount() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
+    );
+
+    const json = await res.json();
+    console.log(json);
+
+    this.setState(Object.assign({ loading: false }, json.pets[0]));
+  }
+
+  render() {
+    const { name, animal, breed, city, state, description } = this.state;
+
+    if (this.state.loading) {
+      return <h2>Loading...</h2>;
+    }
+
+    return (
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal} — ${breed} — ${city}, ${state}`}</h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default withRouter(Details);
